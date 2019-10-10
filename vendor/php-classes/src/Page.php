c42cc6c501e0e859de3d3320ee24233da06ea2c6
Matheus Ferreira Santos Holms <matheus_holms@hotmail.com>
2019-10-10 15:31:13 -0300

    sistema de login

--- vendor/php-classes/src/Page.php
+++ vendor/php-classes/src/Page.php
@@ -16,6 +16,8 @@ class Page {
 
 	public function __construct($opts = array(), $tpl_dir = "/views/"){
 
+		//$this->defaults["data"]["session"] = $_SESSION;
+
 		$this->options = array_merge($this->defaults, $opts);
 
 		$config = array(
@@ -31,7 +33,7 @@ class Page {
 
 		$this->setData($this->options["data"]);
 
-		$this->tpl->draw("header");
+		if ($this->options["header"] === true) $this->tpl->draw("header");
 
 	}
 
@@ -53,7 +55,7 @@ class Page {
 
 	public function __destruct(){
 
-		$this->tpl->draw("footer");
+		if ($this->options["footer"] === true) $this->tpl->draw("footer");
 
 
 	}
