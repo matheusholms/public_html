cf915f6b8665a4e2a4b1b2ca36d0b063378b7a40
Matheus Ferreira Santos Holms <matheus_holms@hotmail.com>
2019-11-22 11:37:07 -0300

    categorias

---
+++ views-cache/categories-menu.b770740758be389a41c06ca720f4dc80.rtpl.php
@@ -0,0 +1,1 @@
+<?php if(!class_exists('Rain\Tpl')){exit;}?><li><a href="/category/7">Apple</a></li><li><a href="/category/3">Google</a></li><li><a href="/category/8">Motorola</a></li><li><a href="/category/10">Samsung</a></li><li><a href="/category/9">Xiaomi</a></li>
--- views-cache/header.bf8ac6a6491b926ea0fa940266a1202f.rtpl.php
+++ views-cache/header.bf8ac6a6491b926ea0fa940266a1202f.rtpl.php
@@ -257,7 +257,7 @@ desired effect
       <li class="header">HEADER</li>
       <!-- Optionally, you can add icons to the links -->
       <li class="active"><a href="/index.php/admin/users"><i class="fa fa-users"></i> <span>Usuários</span></a></li>
-      <li><a href="#"><i class="fa fa-link"></i> <span>Another Link</span></a></li>
+      <li><a href="/index.php/admin/categories"><i class="fa fa-link"></i> <span>Categorias</span></a></li>
       <li class="treeview">
         <a href="#"><i class="fa fa-link"></i> <span>Multilevel</span>
           <span class="pull-right-container">
